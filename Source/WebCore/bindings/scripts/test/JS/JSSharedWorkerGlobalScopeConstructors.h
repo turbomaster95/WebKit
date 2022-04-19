92b87da25e441ba1c29a5404609aa0f7d92bf6de
Deva Midhun <86113389+turbomaster95@users.noreply.github.com>
2022-04-19 11:04:40 +0530

    Delete Source directory

--- Source/WebCore/bindings/scripts/test/JS/JSSharedWorkerGlobalScopeConstructors.h
+++
@@ -1,5 +0,0 @@
-/*
-    This file is generated to inform build scripts that JSSharedWorkerGlobalScopeConstructors.h and
-    JSSharedWorkerGlobalScopeConstructors.cpp were created for SharedWorkerGlobalScopeConstructors.idl, and prevent the build
-    scripts from trying to regenerate JSSharedWorkerGlobalScopeConstructors.h and JSSharedWorkerGlobalScopeConstructors.cpp on every build.
-*/
--- Source/WebCore/bindings/scripts/test/JS/JSTestPartialNamespaceObject.h
+++
@@ -1,5 +0,0 @@
-/*
-    This file is generated to inform build scripts that JSTestPartialNamespaceObject.h and
-    JSTestPartialNamespaceObject.cpp were created for TestPartialNamespaceObject.idl, and prevent the build
-    scripts from trying to regenerate JSTestPartialNamespaceObject.h and JSTestPartialNamespaceObject.cpp on every build.
-*/
